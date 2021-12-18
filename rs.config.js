13595e5b086e0dc0b56d32ce6b20753434ee9889
Austin Howard <ahoward2@unca.edu>
2021-12-17 20:54:37 -0500

    testable config types

--- rs.config.js
+++ rs.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   federatedServerPort: "3001",
+  testArray: [
+    {
+      name: "test1",
+      value: "test1"
+    },
+  ],
+  testObject: {
+    name: "test2",
+    value: "test2"
+  },
+  testInteger: 1,
+  testFunction: function() {
+    return "test3";
+  },
 };
